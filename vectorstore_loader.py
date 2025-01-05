5967572ec5cc5c2ef062eeab1ae3c212476fe8ce
ABDUL QADIR <96516609+AbdulQadirM@users.noreply.github.com>
2025-01-06 03:09:11 +0500

    Update vectorstore_loader.py

--- vectorstore_loader.py
+++ vectorstore_loader.py
@@ -1,12 +1,14 @@
 from langchain_openai import OpenAIEmbeddings
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
+
+
 
 class VectorStoreLoader:
     """
     A class to initialize and load a Chroma vector store.
     """
 
-    def __init__(self):
+    def _init_(self):
         self.vectorstore = None
 
     def initialize_vectorstore(self,folder_path):
@@ -14,7 +16,7 @@ class VectorStoreLoader:
         Initializes a Chroma vector store with OpenAI embeddings.
         """
         embeddings = OpenAIEmbeddings()
-        self.vectorstore = Chroma(embedding_function=embeddings, persist_directory=folder_path)
+        self.vectorstore = FAISS.load_local(folder_path=folder_path,embeddings=embeddings,allow_dangerous_deserialization=True)
 
     def get_vectorstore(self):
         """
@@ -26,8 +28,3 @@ class VectorStoreLoader:
         if self.vectorstore is None:
             raise RuntimeError("Vector store is not initialized. Please call initialize_vectorstore first.")
         return self.vectorstore
-    
-
-    
-
-
