e095906982c67f262f1054c73905e4e7ade9b272
ABDUL QADIR <96516609+AbdulQadirM@users.noreply.github.com>
2025-01-06 03:11:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from vectorstore_loader import VectorStoreLoader
 from langchain_community.document_loaders import PyPDFLoader , TextLoader , Docx2txtLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings , ChatOpenAI
-from langchain_chroma import Chroma
+
+from langchain_community.vectorstores import FAISS
 
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
@@ -70,6 +71,8 @@ async def manage_documents(
         vectorstore = uploader.get_vectorstore()
         return {"status": "success", "message": "Vector store is initialized."}
        
+
+
 
 # Pydantic model to handle user input
 class QueryRequest(BaseModel):
